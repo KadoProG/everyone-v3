87bba41a0953f1c1b4fcf2426be2756e3e4d040e
KadoProG <splatrain@gmail.com>
2024-01-14 12:57:10 +0900

    #15 Googleアナリティクスを導入

--- features/environmentKey.ts
+++ features/environmentKey.ts
@@ -2,6 +2,7 @@ if (
   !process.env.BASE_URL ||
   !process.env.NEXTAUTH_SECRET ||
   !process.env.GITHUB_CLIENT_ID ||
+  !process.env.GA_TRACKING_ID ||
   !process.env.GITHUB_CLIENT_SECRET
 ) {
   throw new Error('環境変数が設定されていません');
@@ -10,6 +11,7 @@ if (
 export const ENVIROMENT_KEY = {
   BASE_URL: process.env.BASE_URL,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  GA_TRACKING_ID: process.env.GA_TRACKING_ID,
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
 } as const;
